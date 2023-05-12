bbc9450a4ab91bb9de635eb5a766d92f0e63ebd0
valiray <133289098+valiray@users.noreply.github.com>
2023-05-11 19:38:50 -0500

    Delete tagging.py

--- tagging.py
+++
@@ -1,5 +0,0 @@
-tagging_system_1 = ""
-tag_list = tagging_system_1.split() # Split the input string by space
-tag_list = [tag.replace('_', ' ') for tag in tag_list] # Replace underscores with spaces in each tag
-tagging_system_2 = ", ".join(tag_list) # Join the list of tags with commas and spaces
-print(tagging_system_2) # Output: "tag 1, tag 2, tag 3"
