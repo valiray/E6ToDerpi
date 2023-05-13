b1319f0e2ef6e5f6d5105f566a0273d17aad0d06
valiray <133289098+valiray@users.noreply.github.com>
2023-05-13 02:07:04 -0500

    Update tagingv3.py

--- tagingv3.py
+++ tagingv3.py
@@ -1,28 +1,12 @@
-import os
+import re
 
-# Check if input file exists, otherwise create it
-if not os.path.exists('input.txt'):
-    with open('input.txt', 'w') as f:
-        pass
-
-# Open input file
 with open('input.txt', 'r') as input_file:
-    # Read the input from input file
-    tagging_system_1 = input_file.read()
+    input_string = input_file.read().strip() # Read the input string from input.txt and remove leading/trailing whitespaces
 
-    # Split the input string by space
-    tag_list = tagging_system_1.split()
+tag_list = input_string.split() # Split the input string by space
+tag_list = [tag.replace('_', ' ') for tag in tag_list] # Replace underscores with spaces in each tag
+tag_list = [re.sub(r'(\d)([a-zA-Z])', r'\1 \2', tag) for tag in tag_list] # Insert space between digits and letters in each tag
+tagging_system_2 = ", ".join(tag_list) # Join the list of tags with commas and spaces
 
-    # Replace underscores with spaces in each tag
-    tag_list = [tag.replace('_', ' ') for tag in tag_list]
-
-    # Join the list of tags with commas and spaces
-    tagging_system_2 = ", ".join(tag_list)
-
-# Open output file
 with open('output.txt', 'w') as output_file:
-    # Write the output to output file
-    output_file.write(tagging_system_2)
-
-# Print confirmation message
-print('Output written to output.txt')
+    output_file.write(tagging_system_2) # Write the output string to output.txt
